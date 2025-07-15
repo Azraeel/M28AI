f59ff1b75dd1cb77d8b7beace033bb3188eb620e
Azraeel <kanyepayton1@gmail.com>
2025-07-15 21:44:00 +0900

    Fix DFTank including Snipers

--- lua/AI/LOUD/M28OtherLOUDCompatibility.lua
+++ lua/AI/LOUD/M28OtherLOUDCompatibility.lua
@@ -183,6 +183,7 @@ function UpdateUnitCategories()
         if M28Utilities.bQuietModActive then
             --(QUIET removes the base exclusion of certain unit categories from lAB/addition to indirect fire, due to these being considered T0.5 units)
             M28UnitInfo.refCategoryIndirect = categories.LAND * categories.MOBILE * categories.INDIRECTFIRE - categories.DIRECTFIRE - M28UnitInfo.refCategoryLandExperimental - M28UnitInfo.refCategoryScathis - categories.UNSELECTABLE - categories.UNTARGETABLE
+            M28UnitInfo.refCategoryDFTank = categories.LAND * categories.MOBILE * categories.DIRECTFIRE - categories.SCOUT - M28UnitInfo.refCategoryMAA - categories.UNSELECTABLE - categories.UNTARGETABLE - categories.SNIPER
             M28UnitInfo.refCategoryLightAttackBot = categories.LAND * categories.DIRECTFIRE * categories.TECH1 * categories.MOBILE
             if categories.uel0106 and categories.url0106 and categories.ual0106 then
                 refCategoryLightAttackBot = categories.uel0106 + categories.url0106 + categories.ual0106
